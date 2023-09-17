4416e01ab7422ea649881b1b73b23f109a1d3211
Shu Jin <sjin32@my.centennialcollege.ca>
2023-09-16 23:56:19 -0400

    lab1 init

--- Lab01/App.xaml.cs
+++ Lab01/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Lab_1
+namespace Lab01
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lab_1
+namespace Lab01
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
