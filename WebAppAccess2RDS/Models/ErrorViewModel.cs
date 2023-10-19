77344769e87e70ed28ff130bef6560bbfdc9578f
Shu Jin <sjin32@my.centennialcollege.ca>
2023-10-18 23:28:52 -0400

    add WebAppAccess2RDS

---
+++ WebAppAccess2RDS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAccess2RDS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebAppAccess2RDS/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppAccess2RDS.Models;
+
+public partial class Login
+{
+    public string LoginName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual Student LoginNameNavigation { get; set; } = null!;
+}
