0dc01264f8c34f0e79879861b87c22363a1ca9c7
sjin <sjin32@my.centennialcollege.ca>
2023-10-31 14:13:22 +0800

    Lab3 Parameter Store OK

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -1,5 +1,8 @@
+using Amazon.SimpleSystemsManagement;
 using Lab03.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 
 namespace Lab03
 {
@@ -7,7 +10,46 @@ namespace Lab03
     {
         public static void Main(string[] args)
         {
+
+            ////get parameters from aws parameter store
+            //var parameterStore = new AmazonSimpleSystemsManagementClient();
+            //var parameterRequest = new Amazon.SimpleSystemsManagement.Model.GetParameterRequest();
+            //parameterRequest.Name = "/Lab3/ConnectionString";
+            //var parameterResponse = parameterStore.GetParameterAsync(parameterRequest).Result;
+            //var connectionString = parameterResponse.Parameter.Value;
+
+          
+
+
+
+
             var builder = WebApplication.CreateBuilder(args);
+             
+
+            var awsKey = builder.Configuration["AWSCredentials:AccesskeyID"];
+            var awsSecret = builder.Configuration["AWSCredentials:Secretaccesskey"];
+
+
+            builder.Configuration.AddSystemsManager("/COMP306_Lab03_Movie_App", new Amazon.Extensions.NETCore.Setup.AWSOptions
+            {
+                Region = Amazon.RegionEndpoint.CACentral1 ,
+                Credentials = new Amazon.Runtime.BasicAWSCredentials(awsKey, awsSecret)
+
+            });
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("connction_Lab03"));
+            connectionStringBuilder.UserID = builder.Configuration["DbUser"];
+            connectionStringBuilder.Password = builder.Configuration["DbPassword"];
+
+
+             
+
+
+
+
+
+
+
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -20,8 +62,15 @@ namespace Lab03
                 options.Cookie.IsEssential = true;
             });
 
+
+
             // Add services to the container.
-            builder.Services.AddDbContext<Lab3MovieWebContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection_local")));
+            //builder.Services.AddDbContext<Lab3MovieWebContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection_local")));
+            builder.Services.AddDbContext<Lab3MovieWebContext>(options => options.UseSqlServer(connectionStringBuilder.ConnectionString));
+
+
+
+
 
             var app = builder.Build();
 
