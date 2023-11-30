0cf2b196644996e6a0914102e53a8a4c4f219807
sjin <sjin32@my.centennialcollege.ca>
2023-11-29 21:41:35 -0500

    lab4 update db

--- COMP306_ShuJin_Project1/Migrations/20231130023832_YourMigrationName.Designer.cs
+++ COMP306_ShuJin_Project1/Migrations/20231130023832_YourMigrationName.Designer.cs
@@ -4,6 +4,7 @@ using COMP306_ShuJin_Project1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace COMP306_ShuJin_Project1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231130023832_YourMigrationName")]
+    partial class YourMigrationName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
